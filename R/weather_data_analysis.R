2d10d0c7c86945171d29a833d3659345fcbaa0ae
yusriy <yusriyp@gmail.com>
2016-07-14 22:58:39 +0800

    Added cumulative rain

--- R/weather_data_analysis.R
+++ R/weather_data_analysis.R
@@ -2,14 +2,17 @@
 # Author: Yusri Yusup, PhD
 # Affiliation: Universiti Sains Malaysia
 # Date created: 2016-06-22
+# Note: R script to analyze MPOB weather station data
 # Version: 1.0
 #
 
 #### Preliminaries ####
 library(openair)
+library(dplyr) # To group the data for cumulative rain calculations
 source('R/vap_deficit.R') # To calculate vapor pressure deficit [kPa]
 source('R/julian_conv.R') # To calculate julian day
 source('R/ET.R') # To calculate evapotranspiration [mm time-1]
+
 
 #### Data import ####
 # Import the data
@@ -30,10 +33,32 @@ w_data$TIMESTAMP <- as.POSIXct(w_data$TIMESTAMP)
 names(w_data)[1] <- 'date'
 rm(i)
 
-
 #### Averaging ####
+# Note that rain is also averaged though the sum is needed
 w_data_monthly <- timeAverage(w_data, avg.time = 'month')
 w_data_daily <- timeAverage(w_data, avg.time = 'day')
+
+#### Cumulative rain (but just for rain) ####
+# Sum the daily rain
+rain_daily <- w_data %>%
+  group_by(year = format(as.POSIXct(cut(date, breaks = 'year')), '%Y'),
+           month = format(as.POSIXct(cut(date, breaks = 'month')), '%m'),
+           day = format(as.POSIXct(cut(date, breaks = 'day')), '%d')) %>%
+  summarize(rain_daily_tot = sum(Rain_mm_Tot, na.rm = TRUE))
+# Add the dates in POSIX format
+date <- paste(rain_daily$year, rain_daily$month, rain_daily$day, sep = '-')
+date <- as.POSIXlt(date)
+rain_daily <- cbind(date, rain_daily)
+
+# Sum the monthly rain
+rain_monthly <- w_data %>%
+  group_by(year = format(as.POSIXct(cut(date, breaks = 'year')), '%Y'),
+           month = format(as.POSIXct(cut(date, breaks = 'month')), '%m')) %>%
+  summarize(rain_monthly_tot = sum(Rain_mm_Tot, na.rm = TRUE))
+date <- paste(rain_monthly$year, rain_monthly$month, '01', sep = '-')
+date <- as.POSIXlt(date)
+rain_monthly <- cbind(date, rain_monthly)
+rm(date)
 
 #### Julian day ####
 # Calculate julian day for hourly data
@@ -44,11 +69,13 @@ rm(jd)
 # Calculate julian day for daily data
 jd <- sapply(w_data_daily$date, julian_conv)
 w_data_daily <- cbind(round(jd), w_data_daily)
+names(w_data_daily)[1] <- 'jd'
 rm(jd)
 
 # Calculate julian day for monthly data
 jd <- sapply(w_data_monthly$date, julian_conv)
 w_data_monthly <- cbind(round(jd), w_data_monthly)
+names(w_data_monthly)[1] <- 'jd'
 rm(jd)
 
 #### Calculate vapor pressure deficit ####
@@ -62,7 +89,7 @@ vpd_monthly <- vap_deficit(w_data_monthly$AirTempC_Avg,
 
 #### Calculate evapotranspiration ####
 # Can only calculate from daily and monthly averaged values
-et_daily <- ET(j = w_data_daily$`round(jd)`,
+et_daily <- ET(j = w_data_daily$jd,
                Tmin = w_data_daily$AirTempC_Min,
                Tmax = w_data_daily$AirTempC_Max,
                solar_rad = w_data_daily$SlrRad_W_Avg,
@@ -72,7 +99,7 @@ et_daily <- ET(j = w_data_daily$`round(jd)`,
                RHmin = w_data_daily$RelHum_Min,
                lat_deg = 5, # Assuming the latitude of the station is at 5 deg
                a = 0.23) # Albedo reference evapotranspiration for grass surface 
-et_monthly <- ET(j = w_data_monthly$`round(jd)`,
+et_monthly <- ET(j = w_data_monthly$jd,
                  Tmin = w_data_monthly$AirTempC_Min,
                  Tmax = w_data_monthly$AirTempC_Max,
                  solar_rad = w_data_monthly$SlrRad_W_Avg,
@@ -89,24 +116,62 @@ w_data_daily <- cbind(w_data_daily, vpd_daily, et_daily)
 w_data_monthly <- cbind(w_data_monthly, vpd_monthly, et_monthly)
 rm(vpd, vpd_daily, vpd_monthly, et_daily, et_monthly)
 
-#### Wind rose ####
-windRose(w_data, ws = 'WindSpd_ms_Avg', wd = 'WindDir_Avg', paddle = FALSE)
+#### Descriptive statistics ####
+summary(w_data)
+summary(w_data_daily)
+summary(w_data_monthly)
 
 #### Plots ####
-# Monthly rain
-plot(w_data_monthly$date,w_data_monthly$Rain_mm_Tot, type = 'o', ylim = c(0, 4),
-     ylab = 'Cumulative rain (mm)', xlab = 'Month', main = 'Monthly rain')
-# Temperature
-plot(w_data_monthly$date,w_data_monthly$AirTempC_Avg, 
-     type = 'o',
-     ylab = 'Temperature (C)', xlab = 'Month', main = 'Monthly temperature')
-# Vapor pressure deficit
-plot(w_data_monthly$date,w_data_monthly$vpd_monthly, 
-     type = 'o',
-     ylab = 'VPD (kPa)', xlab = 'Month', main = 'Monthly vapor pressure deficit')
-# Evapotranspiration
-plot(w_data_monthly$date,w_data_monthly$et_monthly, 
-     type = 'o',
-     ylab = 'ET (mm day-1)', xlab = 'Month', main = 'Monthly evapotranspiration')
 
+## Change your "settings" here
+# The default date format for POSIX data types
+format_date <- '%Y-%m-%d %H:%M:%S'
+# You can specify the start and end dates here to plot
+date1 <- '2011-04-01 00:00:00'
+date2 <- '2011-12-30 00:00:00'
+
+## Time series plot
+# Daily cumulative rain
+plot(rain_daily$date, rain_daily$rain_daily_tot, type = 'o', 
+     xaxt = 'n', xlab = 'Date',
+     ylab = 'Total rain (mm)',
+     xlim = c(as.POSIXct(date1, format = format_date), 
+              as.POSIXct(date2, format = format_date)))
+axis.POSIXct(side = 1, at = rain_daily$date, 
+             labels = format(rain_daily$date, '%m/%d'))
+
+# Monthly cumulative rain
+plot(rain_monthly$date, rain_monthly$rain_monthly_tot, type = 'o', 
+     xaxt = 'n', xlab = 'Month',
+     ylab = 'Total rain (mm)',
+     xlim = c(as.POSIXct(date1, format = format_date), 
+              as.POSIXct(date2, format = format_date)))
+axis.POSIXct(side = 1, at = rain_monthly$date, 
+             labels = format(rain_monthly$date, '%m'))
+
+
+## Monthly averaged 
+# Monthly averaged temperature
+plot(w_data_monthly$date,w_data_monthly$AirTempC_Avg, 
+     type = 'o', xaxt ='n',
+     ylab = 'Temperature (C)', xlab = 'Month', main = 'Monthly temperature')
+axis.POSIXct(side = 1, at = w_data_monthly$date, 
+             labels = format(w_data_monthly$date, '%Y/%m'))
+
+# Monthly averaged vapor pressure deficit
+plot(w_data_monthly$date,w_data_monthly$vpd_monthly, 
+     type = 'o', xaxt ='n',
+     ylab = 'VPD (kPa)', xlab = 'Month', main = 'Monthly vapor pressure deficit')
+axis.POSIXct(side = 1, at = w_data_monthly$date, 
+             labels = format(w_data_monthly$date, '%Y/%m'))
+
+# Monthly averaged evapotranspiration
+plot(w_data_monthly$date,w_data_monthly$et_monthly, 
+     type = 'o', xaxt ='n',
+     ylab = 'ET (mm day-1)', xlab = 'Month', main = 'Monthly evapotranspiration')
+axis.POSIXct(side = 1, at = w_data_monthly$date, 
+             labels = format(w_data_monthly$date, '%Y/%m'))
+
+#### Wind rose ####
+windRose(w_data, ws = 'WindSpd_ms_Avg', wd = 'WindDir_Avg', paddle = FALSE)
 
